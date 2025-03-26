0cafc925a1eb00ebebcc9b01cf628d7d1cf1f87f
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-03-26 14:05:22 +0330

    final day1

---
+++ day1/main.py
@@ -0,0 +1,27 @@
+#print function
+"""
+print("Day 1 Python print Function \n The function is declared like this : \n print(what('what to print'))")
+print("Hello" + "   "+ "amirabbas")
+print("Hello" + input("What is your name :") + " ! ")
+
+"""
+
+#len
+"""
+len_number = len (input("What is your name :"))
+print(len_number)
+
+"""
+
+#project
+
+print("welcome to the band name generator")
+
+City = input("Which city did you grow up in ?")
+print(City)
+
+Pet = input("what is the name of pet :")
+print(Pet)
+
+print(f" your band name could be: {City} and {Pet} ")
+
